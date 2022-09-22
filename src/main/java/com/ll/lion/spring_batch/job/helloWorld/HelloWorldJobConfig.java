12b28c81d6bab89fb34951a52f958e4c441bebb2
박다정 <ekwjd990913@gmail.com>
2022-09-22 10:33:07 +0900

    fixed untracked files

---
+++ src/main/java/com/ll/lion/spring_batch/job/helloWorld/HelloWorldJobConfig.java
@@ -0,0 +1,46 @@
+package com.ll.lion.spring_batch.job.helloWorld;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job helloWorldJob() {
+        //jobBuilderFactory를 통해 JOB을 생성한다.
+        return jobBuilderFactory.get("helloWorldJob")
+                .start(helloWorldStep1())
+                .build();
+//       스텝이 하나짜리인 job이다.
+    }
+
+    @Bean
+    public Step helloWorldStep1() {
+        return stepBuilderFactory.get("helloWorldStep1")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return ((contribution, chunkContext) -> {
+            System.out.println("hello, world!");
+
+            return RepeatStatus.FINISHED;
+        });
+    }
+}
