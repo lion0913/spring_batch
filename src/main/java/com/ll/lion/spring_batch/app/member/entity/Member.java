aea8acceefcf847052feedcad53e5376a62d18b3
박다정 <ekwjd990913@gmail.com>
2022-09-26 10:52:43 +0900

    [고객] 고객 테스트 데이터 생성

---
+++ src/main/java/com/ll/lion/spring_batch/app/member/entity/Member.java
@@ -0,0 +1,20 @@
+package com.ll.lion.spring_batch.app.member.entity;
+
+
+import com.ll.lion.spring_batch.app.base.entity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ src/main/java/com/ll/lion/spring_batch/app/member/service/MemberService.java
@@ -0,0 +1,26 @@
+package com.ll.lion.spring_batch.app.member.service;
+
+
+import com.ll.lion.spring_batch.app.member.entity.Member;
+import com.ll.lion.spring_batch.app.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member join(String username, String password, String email) {
+        Member member = Member
+                .builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .build();
+
+        memberRepository.save(member);
+
+        return member;
+    }
+}
