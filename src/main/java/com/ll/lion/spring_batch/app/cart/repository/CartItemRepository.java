8c473a14aac2312960bbe0819c939fe1a3cda4be
박다정 <ekwjd990913@gmail.com>
2022-09-26 13:27:45 +0900

    [장바구니] 동일 상품 여러개 추가시 quantity개수만 UPDate되도록 처리

--- src/main/java/com/ll/lion/spring_batch/app/cart/repository/CartItemRepository.java
+++ src/main/java/com/ll/lion/spring_batch/app/cart/repository/CartItemRepository.java
@@ -4,4 +4,5 @@ import com.ll.lion.spring_batch.app.cart.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    CartItem findByMemberIdAndProductOptionId(Long id, Long optionId);
 }
--- src/main/java/com/ll/lion/spring_batch/app/cart/service/CartService.java
+++ src/main/java/com/ll/lion/spring_batch/app/cart/service/CartService.java
@@ -13,7 +13,17 @@ import org.springframework.stereotype.Service;
 public class CartService {
     private final CartItemRepository cartItemRepository;
 
-    public void addItem(Member member, ProductOption productOption, int quantity) {
+    public CartItem addItem(Member member, ProductOption productOption, int quantity) {
+
+        CartItem oldCartItem = cartItemRepository.findByMemberIdAndProductOptionId(member.getId(), productOption.getId());
+
+        if(oldCartItem != null) {
+            oldCartItem.setQuantity(oldCartItem.getQuantity() + quantity);
+            cartItemRepository.save(oldCartItem);
+
+            return oldCartItem;
+        }
+
         CartItem cartItem = CartItem.builder()
                 .member(member)
                 .productOption(productOption)
@@ -21,5 +31,6 @@ public class CartService {
                 .build();
 
         cartItemRepository.save(cartItem);
+        return cartItem;
     }
 }
