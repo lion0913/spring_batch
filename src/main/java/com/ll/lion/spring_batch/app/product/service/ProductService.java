11318e499a2bd417d0654828e9b9ac1438b9276a
박다정 <ekwjd990913@gmail.com>
2022-09-26 15:29:16 +0900

    [상품] 판매가격 1000원단위로 담기게 설정

--- src/main/java/com/ll/lion/spring_batch/app/product/service/ProductService.java
+++ src/main/java/com/ll/lion/spring_batch/app/product/service/ProductService.java
@@ -15,7 +15,7 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public Product create(String name, int salePrice, int wholesalePrice, String makerShopName, List<ProductOption> options) {
-        int price = (int)Math.ceil(wholesalePrice * 1.6);
+        int price = (int)Math.ceil(wholesalePrice * 1.6) / 100 * 100;
 
         Product product = Product.builder()
                 .name(name)
